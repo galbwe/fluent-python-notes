7a82b9b711546af2850f6b502faa4fedd732fece
Wes Galbraith <galbwe92@gmail.com>
2020-08-20 09:01:53 -0600

    Add notes on slots, overriding class attributes

--- fluent_python/ch9/test_vector.py
+++ fluent_python/ch9/test_vector.py
@@ -1,7 +1,7 @@
 import math
 import pytest
 
-from .vector import Vector2d
+from .vector import Vector2d, ShortVector2d
 
 
 class TestVector2d:
@@ -32,3 +32,18 @@ class TestVector2d:
     def test_bool(self, vec_1, zero_vec):
         assert bool(vec_1) is True
         assert bool(zero_vec) is False
+
+    def test_frombytes(self, vec_1):
+        octets = bytes(vec_1)
+        v2 = Vector2d.frombytes(octets)
+        assert v2 == vec_1
+
+
+class TestShortVector2d:
+    @pytest.fixture
+    def short_vec_1(self):
+        return ShortVector2d(1/11, 1/27)
+    
+    def test_bytes(self, short_vec_1):
+        assert len(bytes(short_vec_1)) == 9
+
--- fluent_python/ch9/vector.py
+++ fluent_python/ch9/vector.py
@@ -1,10 +1,21 @@
+from array import array
 import math
 
 
 class Vector2d:
+    typecode = 'd'  # needed to convert to/from bytes
+
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __repr__(self):
         return f'{self.__class__.__name__}(x={self.x},y={self.y})'
@@ -12,11 +23,15 @@ class Vector2d:
     def __str__(self):
         return f'<{self.x}, {self.y}>'
 
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
     def __iter__(self):
         return (element for element in (self.x, self.y))
     
     def __bytes__(self):
-        pass
+        return (bytes([ord(self.typecode)]) +
+            bytes(array(self.typecode, self)))
 
     def __eq__(self, other: 'Vector2d'):
         return self.x == other.x and self.y == other.y
@@ -26,3 +41,13 @@ class Vector2d:
 
     def __bool__(self):
         return abs(self) > 0
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+
+class ShortVector2d(Vector2d):
+    typecode = 'f'
